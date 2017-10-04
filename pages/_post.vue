5363fcb36de78a8288d24f5047d796f92c5f4a7b
Luca D'Amico <menteora@gmail.com>
2017-10-04 14:38:46 +0200

    added card post

--- pages/_post.vue
+++ pages/_post.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-  <v-parallax :src="'/images/'+post.image.feature" v-if="post.image.feature"></v-parallax>
   <v-layout column justify-center align-center>
-  <v-flex xs12 md7>
-    <h2>{{ post.title }}</h2>
-  <nuxtent-body :body="post.body" />
-</v-flex>
-</v-layout>
-</div>
+    <v-flex xs12 md7>
+      <v-card>
+        <v-card-media :src="'/images/'+post.image.feature" v-if="post.image.feature"  height="300px">
+        </v-card-media>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">{{ post.title }}</h3>
+            <nuxtent-body :body="post.body" />
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Share</v-btn>
+          <v-btn flat class="orange--text">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
-
 <script>
 export default {
   asyncData: async ({ app, route, payload }) => ({
