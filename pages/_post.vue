a0594c905ce3f6b35aa556e62046adc6f654aad4
Luca D'Amico <menteora@gmail.com>
2017-10-21 22:47:54 +0200

    fixed post author

--- pages/_post.vue
+++ pages/_post.vue
@@ -11,132 +11,126 @@
     </v-flex>
   </v-layout>
   <v-layout row wrap>
-    <v-flex d-flex xs12 sm4 md3 order-xs2 order-md1>
+    <v-flex d-flex xs12 sm4 md3>
       <v-layout column>
-        <v-flex d-flex>
-          <v-card>
-            <v-card-title>
-              <v-flex xs4>
-              <v-avatar :tile=false class="grey lighten-4" size="100px">
-                <img :src="'/images/'+post.author+'.png'" alt="avatar">
-              </v-avatar>
-            </v-flex>
-              <v-flex xs4>
-              <h2>by {{post.author}}</h2>
-              </v-flex>
-            </v-card-title>
-
-  <v-card-text>
-<p><v-icon>label</v-icon> {{post.category}}</p>
-<p><v-icon>loyalty</v-icon>  {{post.tags}}</p>
-<p><v-icon>event</v-icon> {{post.date}}</p>
-  </v-card-text>
-<v-divider></v-divider>
-            <v-card-actions>
-              <social-sharing :to="post.permalink" :title="post.title" :description="post.description" :hashtags="post.tags.toString()" twitter-user="dinamicamente" inline-template>
-                <div>
-                  <network network="facebook">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator" class="blue--text">
-                        <v-icon large>fa-facebook-official</v-icon>
-                      </v-btn>
-                      <span>Facebook</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="googleplus">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator" class="red--text">
-                        <v-icon large>fa-google-plus</v-icon>
-                      </v-btn>
-                      <span>Google+</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="linkedin">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator" class="blue--text">
-                        <v-icon large>fa-linkedin</v-icon>
-                      </v-btn>
-                      <span>Linkedin</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="telegram">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator" class="blue--text">
-                        <v-icon large>fa-telegram</v-icon>
-                      </v-btn>
-                      <span>Telegram</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="twitter">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator" class="blue--text">
-                        <v-icon large>fa-twitter</v-icon>
-                      </v-btn>
-                      <span>Twitter</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="whatsapp">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator" class="green--text">
-                        <v-icon large>fa-whatsapp</v-icon>
-                      </v-btn>
-                      <span>Whatsapp</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="email">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator">
-                        <v-icon large>fa-envelope</v-icon>
-                      </v-btn>
-                      <span>E-mail</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="pinterest">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator" class="red--text">
-                        <v-icon large>fa-pinterest</v-icon>
-                      </v-btn>
-                      <span>Pinterest</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="reddit">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator">
-                        <v-icon large>fa-reddit</v-icon>
-                      </v-btn>
-                      <span>Reddit</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="skype">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator" class="blue--text">
-                        <v-icon large>fa-skype</v-icon>
-                      </v-btn>
-                      <span>Skype</span>
-                    </v-tooltip>
-                  </network>
-                  <network network="sms">
-                    <v-tooltip top>
-                      <v-btn large flat slot="activator">
-                        <v-icon large>fa-commenting-o</v-icon>
-                      </v-btn>
-                      <span>Sms</span>
-                    </v-tooltip>
-                  </network>
-                </div>
-              </social-sharing>
-            </v-card-actions>
-          </v-card>
+        <v-flex pa-3>
+          <v-flex>
+            <v-avatar :tile=false class="grey lighten-4" size="100px">
+              <img :src="'/images/'+post.author+'.png'" alt="avatar">
+            </v-avatar>
+          </v-flex>
+          <v-flex>
+            <h2>by {{post.author}}</h2>
+          </v-flex>
+          <v-flex>
+            <p><v-icon>label</v-icon> {{ post.category }}</p>
+            <p><v-icon>loyalty</v-icon> {{ post.tags.toString() }}</p>
+            <p><v-icon>event</v-icon> {{ post.date }}</p>
+        </v-flex>
         </v-flex>
       </v-layout>
     </v-flex>
-    <v-flex d-flex xs12 sm8 md9 order-xs1 order-md2>
-        <v-card>
-          <v-card-text>
-            <h1 class="display-1">{{ post.title }}</h1>
-            <nuxtent-body :body="post.body" />
-          </v-card-text>
-        </v-card>
+    <v-flex xs12 sm7 md8>
+      <v-card>
+        <v-card-text>
+          <h1 class="display-1">{{ post.title }}</h1>
+          <nuxtent-body :body="post.body" />
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex d-flex xs12 sm1 md1>
+      <social-sharing :to="post.permalink" :title="post.title" :description="post.description" :hashtags="post.tags.toString()" twitter-user="dinamicamente" inline-template>
+        <div>
+          <network network="facebook">
+            <v-tooltip top>
+              <v-btn large flat slot="activator" class="blue--text">
+                <v-icon large>fa-facebook-official</v-icon>
+              </v-btn>
+              <span>Facebook</span>
+            </v-tooltip>
+          </network>
+          <network network="googleplus">
+            <v-tooltip top>
+              <v-btn large flat slot="activator" class="red--text">
+                <v-icon large>fa-google-plus</v-icon>
+              </v-btn>
+              <span>Google+</span>
+            </v-tooltip>
+          </network>
+          <network network="linkedin">
+            <v-tooltip top>
+              <v-btn large flat slot="activator" class="blue--text">
+                <v-icon large>fa-linkedin</v-icon>
+              </v-btn>
+              <span>Linkedin</span>
+            </v-tooltip>
+          </network>
+          <network network="telegram">
+            <v-tooltip top>
+              <v-btn large flat slot="activator" class="blue--text">
+                <v-icon large>fa-telegram</v-icon>
+              </v-btn>
+              <span>Telegram</span>
+            </v-tooltip>
+          </network>
+          <network network="twitter">
+            <v-tooltip top>
+              <v-btn large flat slot="activator" class="blue--text">
+                <v-icon large>fa-twitter</v-icon>
+              </v-btn>
+              <span>Twitter</span>
+            </v-tooltip>
+          </network>
+          <network network="whatsapp">
+            <v-tooltip top>
+              <v-btn large flat slot="activator" class="green--text">
+                <v-icon large>fa-whatsapp</v-icon>
+              </v-btn>
+              <span>Whatsapp</span>
+            </v-tooltip>
+          </network>
+          <network network="email">
+            <v-tooltip top>
+              <v-btn large flat slot="activator">
+                <v-icon large>fa-envelope</v-icon>
+              </v-btn>
+              <span>E-mail</span>
+            </v-tooltip>
+          </network>
+          <network network="pinterest">
+            <v-tooltip top>
+              <v-btn large flat slot="activator" class="red--text">
+                <v-icon large>fa-pinterest</v-icon>
+              </v-btn>
+              <span>Pinterest</span>
+            </v-tooltip>
+          </network>
+          <network network="reddit">
+            <v-tooltip top>
+              <v-btn large flat slot="activator">
+                <v-icon large>fa-reddit</v-icon>
+              </v-btn>
+              <span>Reddit</span>
+            </v-tooltip>
+          </network>
+          <network network="skype">
+            <v-tooltip top>
+              <v-btn large flat slot="activator" class="blue--text">
+                <v-icon large>fa-skype</v-icon>
+              </v-btn>
+              <span>Skype</span>
+            </v-tooltip>
+          </network>
+          <network network="sms">
+            <v-tooltip top>
+              <v-btn large flat slot="activator">
+                <v-icon large>fa-commenting-o</v-icon>
+              </v-btn>
+              <span>Sms</span>
+            </v-tooltip>
+          </network>
+        </div>
+      </social-sharing>
     </v-flex>
   </v-layout>
 </v-container>
