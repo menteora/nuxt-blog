4d75e66d439a8fd4cb093e98893e3876cf606f95
Luca D'Amico <menteora@gmail.com>
2017-10-25 09:41:49 +0200

    added external file config

--- deploy.js
+++ deploy.js
@@ -1,14 +1,15 @@
+var deployConfig = require('config').get('deploy')
 var FtpDeploy = require('ftp-deploy')
 var ftpDeploy = new FtpDeploy()
 const path = require('path')
 
 var config = {
-  username: 'myusername',
-  password: 'mypassword', // optional, prompted if none given
-  host: 'ftp.myhost.com',
-  port: 21,
-  localRoot: path.join(__dirname, '/dist'),
-  remoteRoot: '/public_html/nuxt/'
+  username: deployConfig.get('username'),
+  password: deployConfig.get('password'), // optional, prompted if none given
+  host: deployConfig.get('host'),
+  port: deployConfig.get('port'),
+  localRoot: path.join(__dirname, deployConfig.get('localRoot')),
+  remoteRoot: deployConfig.get('remoteRoot')
 }
 
 ftpDeploy.deploy(config, function (err) {
