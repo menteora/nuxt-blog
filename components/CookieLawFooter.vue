95f5e3c5a936a17d763035414592cdea871f7e3f
Luca D'Amico <menteora@gmail.com>
2017-12-06 20:07:59 +0100

    added cookielaw with emit event

--- components/CookieLawFooter.vue
+++ components/CookieLawFooter.vue
@@ -1,8 +1,7 @@
 <template>
   <footer>
-    <cookie-law theme="dark-lime" buttonText="Accetta">
+    <cookie-law theme="dark-lime" buttonText="Accetta" v-on:accept="$store.commit('acceptCookie')">
         <div slot="message">
-          <button v-on:click="$store.commit('acceptCookie')">cookieAccepted</button>
           Questo sito utilizza i cookie per fornire alcuni servizi. Accetta per avere una migliore esperienza di navigazione. <router-link to="policy">Approfondisci</router-link>
         </div>
     </cookie-law>
