b4918d6f1eab5abb9752c3b45a003fa14a584f24
Luca D'Amico <menteora@gmail.com>
2019-05-09 15:54:24 +0200

    feat: update vue-moment

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -1,58 +1,68 @@
 <template>
-<div>
-  <v-layout column justify-center align-center>
-    <nuxt-page-logo />
-  </v-layout>
-  <div v-for="tag in tags" :key="tag">
-    <h2 class="headline">{{tag}}</h2>
-    <v-container fluid grid-list-xl>
-      <v-layout row wrap>
-        <v-flex xs12 md3 v-for="post in posts" v-if="post.tags.indexOf(tag) > -1" :key="post.title">
-          <v-card class="my-3" hover v-on:click.capture="go(post.path)">
-            <v-img class="white--text" height="150px" :src="'/images/'+post.image.feature" v-if="post.image.feature">
-            </v-img>
-            <v-card-title>{{post.title}}</v-card-title>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
+  <div>
+    <v-layout column justify-center align-center>
+      <nuxt-page-logo/>
+    </v-layout>
+    <div v-for="tag in tags" :key="tag">
+      <h2 class="headline">{{tag}}</h2>
+      <v-container fluid grid-list-xl>
+        <v-layout row wrap>
+          <v-flex
+            xs12
+            md3
+            v-for="post in posts"
+            v-if="post.tags.indexOf(tag) > -1"
+            :key="post.title"
+          >
+            <v-card class="my-3" hover v-on:click.capture="go(post.path)">
+              <v-img
+                class="white--text"
+                height="150px"
+                :src="'/images/'+post.image.feature"
+                v-if="post.image.feature"
+              ></v-img>
+              <v-card-title>{{post.title}}</v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-  import NuxtPageLogo from '~/components/PageLogo.vue'
-  var _ = require('underscore')
+import NuxtPageLogo from "~/components/PageLogo.vue";
+var _ = require("underscore");
 
-  export default {
-    components: {
-      NuxtPageLogo
-    },
-    asyncData: async ({
-      app,
-      route
-    }) => ({
-      posts: await app.$content('/news').query({ exclude: 'body' }).getAll()
-    }),
-    data: function () {
-      return {
-        tags: []
+export default {
+  components: {
+    NuxtPageLogo
+  },
+  asyncData: async ({ app, route }) => ({
+    posts: await app
+      .$content("/news")
+      .query({ exclude: "body" })
+      .getAll()
+  }),
+  data: function() {
+    return {
+      tags: []
+    };
+  },
+  created: function() {
+    var t = [];
+    for (var i = 0; i < this.posts.length; i++) {
+      for (var n = 0; n < this.posts[i].tags.length; n++) {
+        t.push(this.posts[i].tags[n]);
       }
-    },
-    created: function () {
-      var t = []
-      for (var i = 0; i < this.posts.length; i++) {
-        for (var n = 0; n < this.posts[i].tags.length; n++) {
-          t.push(this.posts[i].tags[n])
-        }
-      }
-      this.tags = _.uniq(t)
-    },
-    methods: {
-      go: function (url) {
-        this.$nuxt.$router.push(url)
-      }
-    },
-    filters: {}
-  }
+    }
+    this.tags = _.uniq(t);
+  },
+  methods: {
+    go: function(url) {
+      this.$nuxt.$router.push(url);
+    }
+  },
+  filters: {}
+};
 </script>
--- plugins/vue-moment.js
+++ plugins/vue-moment.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import Moment from 'vue-moment'
-import MomentLocale from 'moment/locale/it'
+import VueMoment from 'vue-moment'
+import moment from 'moment'
+import 'moment/locale/it' // whatever you import here will be set as default, no need to import all locales you intend to use
 
-Vue.use(Moment, {
-  MomentLocale
-})
+Vue.use(VueMoment, { moment })
+/* Sync moment with I18n 
+https://gist.github.com/oliviaisarobot/8203e062a58b9fd4762becabfd7f46d5
+*/
